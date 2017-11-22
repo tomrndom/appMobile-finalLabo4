941c0d37884ecea9dd3edc70d3c2ee227b6c78c5
Tomás Castillo <tcastillo@r1group.co>
2017-11-22 15:19:31 -0300

    first services

---
+++ src/shared/services/articulo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/toPromise';
+
+import { BASE_URL, API_VERSION } from './lb.base.url'
+import { LoopBackConfig, LoopBackFilter, Articulo, ArticuloApi, Rubro } from './lbsdk/index'
+
+@Injectable()
+export class ArticuloService {
+
+    public articuloActual: Articulo;
+
+    constructor(private articuloService: ArticuloApi) {
+    }
+
+    getAll(filtro: any = {}): Observable<Articulo[]> {
+        return this.articuloService.find(filtro);
+    }
+
+    getbyId(id: number): Observable<Articulo> {
+        return this.articuloService.findById(id);
+    }
+
+    getAllByRubro(rubro: Rubro): Observable<Articulo[]> {
+        return this.articuloService.find({ where: { idRubro: rubro.idRubro } });
+    }    
+
+}
---
+++ src/shared/services/lb.base.url.ts
@@ -0,0 +1,8 @@
+export const BASE_URL = 'http://localhost:3000';
+export const API_VERSION = 'api';
+
+// export const BASE_URL = 'http://172.21.30.75:8080';
+// export const BASE_URL = 'http://localhost:8080';
+
+// export const BASE_URL = 'http://docker-testing.tnevada.com.ar:8080';
+// export const BASE_URL = 'http://docker-produccion.tnevada.com.ar:8080';
