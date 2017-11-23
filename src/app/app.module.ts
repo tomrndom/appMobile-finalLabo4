e3c3fee162c70031e043ca0ef5d0082b289a8241
Tomás Castillo <tcastillo@r1group.co>
2017-11-23 08:38:37 -0300

    imports in the app.module, index created for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+// Pages
 import { PedidosPage } from '../pages/pedidos/pedidos';
 import { NuevoPage } from '../pages/nuevo/nuevo';
 import { SincronizarPage } from '../pages/sincronizar/sincronizar';
@@ -10,6 +11,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Services
+import { ArticuloService } from '../shared/services/articulo.service';
+import { ClienteService } from '../shared/services/cliente.service';
+import { DomicilioService } from '../shared/services/domicilio.service';
+import { PedidoVentaService } from '../shared/services/pedidoVenta.service';
+import { PedidoVentaDetalleService } from '../shared/services/pedidoVentaDetalle.service';
+import { RubroService } from '../shared/services/rubro.service';
 
 @NgModule({
   declarations: [
@@ -34,7 +43,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ArticuloService,
+    ClienteService,
+    DomicilioService,
+    PedidoVentaService,
+    PedidoVentaDetalleService,
+    RubroService
   ]
 })
 export class AppModule { }
---
+++ src/shared/services/index.ts
@@ -0,0 +1,6 @@
+export * from './articulo.service';
+export * from './cliente.service';
+export * from './domicilio.service';
+export * from './pedidoVenta.service';
+export * from './pedidoVentaDetalle.service';
+export * from './rubro.service';
